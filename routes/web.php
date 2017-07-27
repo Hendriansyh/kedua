10061f7a74c8971aa57366a1bd6474b21f9aee13
Hendriansyh <151610207@smkassalaam.sch.id>
2017-07-27 16:48:31 +0700

    Seed

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,48 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/about', function ()
+{
+	return '<h1> Hello</h1>'
+	.'Selamat datang diwebapp saya</br>'
+	.'Laravel, emang keren.';
+});
+
+Route::get('/latihan', function ()
+{
+	return view('about');
+});
+
+Route::get('/latihan/1', function ()
+{
+	return view('1');
+});
+
+
+Route::get('/latihan/2', function ()
+{
+	return view('2');
+});
+
+
+Route::get('/latihan/3', function ()
+{
+	return view('3');
+});
+
+Route::get('latihan/{nama}/{sekolah}/{umur}', function($nama,$sekolah,$umur)
+{
+	return 'Nama Saya: '.$nama.'</br>
+	Saya Sekolah Di :'.$sekolah.'</br>
+	Umur saya sekitar : '.$umur;
+});
+
+Route::get('/about/hendri', function()
+{
+	$a=' Asep';
+	return "Ini Halaman About" . $a;
 });
